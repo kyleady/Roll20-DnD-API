03719c587df4dc87252e890122f61b0c1cdfc973
Kyle Ady <kyledavidady@gmail.com>
2018-11-10 08:58:32 -0800

    Players can add AddCounter. Death Sv as an alternative to Death Save

--- Scripts/DnD/Prototypes/INKTest/INKTest.js.actions.js
+++ Scripts/DnD/Prototypes/INKTest/INKTest.js.actions.js
@@ -1,5 +1,5 @@
 INKTest.actions = () => [
   { Name: 'initiative', Alternates: ['init'], NPCName: 'npc_init' },
   { Name: 'hit_dice', NPCName: 'npc_hit_dice' },
-  { Name: 'death_save', NPCName: 'npc_death_save' },
+  { Name: 'death_save', Alternates: ['death sv'], NPCName: 'npc_death_save' },
 ];
