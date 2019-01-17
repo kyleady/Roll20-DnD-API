1af76e29f44da97d906e639bd600d93bfc047fe8
Kyle Ady <kyledavidady@gmail.com>
2019-01-16 22:02:57 -0800

    Expand !dam to work with many aliases. Allow !dam to specify if hit is a crit.

--- Scripts/DnD/API Commands/Damage.js
+++ Scripts/DnD/API Commands/Damage.js
@@ -1,5 +1,6 @@
 const INKDamage = (matches, msg) => {
-  const includeCrit = matches[1] != '';
+  const includeCrit = /crit|critical/i.test(matches[1]);
+  const damageType = matches[2].toUpperCase() || '';
   eachCharacter(msg, (character, graphic) => {
     const bar = 'bar1';
     const currentHealth = Number(graphic.get(`${bar}_value`));
@@ -9,17 +10,42 @@ const INKDamage = (matches, msg) => {
     if(includeCrit) totalDmg += state.INK_DATA.crit || 0;
     graphic.set(`${bar}_value`, currentHealth - totalDmg);
     const percent = Math.round(totalDmg * 100 / maxHealth);
+    damageFx(graphic, damageType)
     whisper(`${graphic.get('name')}: ${percent}%`);
   });
 }
 
-const INKDamageQuery = (matches, msg) => {
+const INKDamageQuery = (matches, msg, options) => {
+  options = options || {};
   const totalDmg = state.INK_DATA.dmg || 0;
   const totalCrit = state.INK_DATA.crit || 0;
-  whisper(`Dmg: ${totalDmg}, Crit: ${totalCrit}`);
+  whisper(`Dmg: ${totalDmg}, Crit: ${totalCrit}`, { speakingTo: msg.playerid, gmEcho: options.gmEcho });
+}
+
+const INKDamageModifier = (matches, msg) => {
+  const type = /(crit|critical)/i.test(matches[1]) ? 'crit' : 'dam';
+  const operator = matches[2];
+  const value = Number(matches[3]);
+
+  const old_value = state.INK_DATA[type] || 0;
+  if(operator == '+') {
+    const new_value = old_value + value;
+  } else if(operator == '-') {
+    const new_value = old_value - value;
+  } else if(operator == '*') {
+    const new_value = old_value * value;
+  } else if(operator == '/') {
+    const new_value = old_value / value;
+  } else if(operator == '') {
+    const new_value = value;
+  }
+
+  state.INK_DATA[type] = Math.round(new_value);
+  INKDamageQuery(matches, msg, { gmEcho: true });
 }
 
 on('ready', () => {
-  CentralInput.addCMD(/^!\s*dmg\s*(|crit|critical)\s*$/i, INKDamage);
-  CentralInput.addCMD(/^!\s*dmg\s*\?\s*$/i, INKDamageQuery);
+  CentralInput.addCMD(/^!\s*(dmg|dam|damage|attack|crit|critical)\s*(I|X|R|E|)\s*$/i, INKDamage, true);
+  CentralInput.addCMD(/^!\s*(dmg|dam|damage|attack|crit|critical)\s*\?\s*$/i, INKDamageQuery, true);
+  CentralInput.addCMD(/^!\s*(dmg|dam|damage|attack|crit|critical)\s*(\+|-|\*|\/|)\s*=?\s*(\d+.?\d*)\s*$/, INKDamageModifier, true)
 });
